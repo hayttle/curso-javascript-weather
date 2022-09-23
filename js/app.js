a3ec9d3f3071393b6ff408f3467185c815628250
hayttle <hayttle@gmail.com>
2022-09-22 22:20:05 -0300

    updated

--- js/app.js
+++ js/app.js
@@ -7,27 +7,40 @@ const cityCard = document.querySelector("[data-js='city-card']");
 let timeImg = document.querySelector("[data-js='time']");
 const timeIconContainer = document.querySelector("[data-js='time-icon'");
 
-cityForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  const inputValue = event.target.city.value;
-  const [{Key, LocalizedName}] = await getCityData(inputValue);
-  const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeather(Key);
-  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg">`;
-
+const showCityCard = () => {
   if (cityCard.classList.contains("d-none")) {
     cityCard.classList.remove("d-none");
   }
+};
 
-  if (IsDayTime) {
-    timeImg.src = "./src/day.svg";
-  } else {
-    timeImg.src = "./src/night.svg";
-  }
+const showCityWeatherInfo = async (cityName) => {
+  const [{Key, LocalizedName}] = await getCityData(cityName);
+  const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeather(Key);
+  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg">`;
+  timeImg.src = IsDayTime ? "./src/day.svg" : "./src/night.svg";
   timeIconContainer.innerHTML = timeIcon;
   cityNameContainer.textContent = LocalizedName;
   cityWeatherContainer.textContent = WeatherText;
   cityTemperatureContainer.textContent = Math.floor(Temperature.Metric.Value);
+  showCityCard();
+};
 
+const showLocalStorageCity = () => {
+  const city = localStorage.getItem("city");
+  if (city) {
+    showCityWeatherInfo(city);
+  }
+};
+
+cityForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const inputValue = event.target.city.value;
+
+  showCityWeatherInfo(inputValue);
+
+  localStorage.setItem("city", inputValue);
   cityForm.reset();
 });
+
+showLocalStorageCity();
